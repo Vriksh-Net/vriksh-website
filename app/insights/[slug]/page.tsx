11cae2b6007182b4bd5cf734b66c8fb922c5d5f4
ritikdevelop <hrithikrana216@gmail.com>
2025-05-22 13:16:25 +0530

    chore: add a blank line for improved code readability in InsightPage component

--- app/insights/[slug]/page.tsx
+++ app/insights/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { getArticleBySlug, type Article } from "@/lib/insight";
 import InsightClientPage from "./InsightClientPage";
 
+
 export default async function InsightPage(props: any) {
   const article: Article | null = await getArticleBySlug(props.params.slug);
 
